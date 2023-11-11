a3cfd830bab2b46d6cce11efcf3c57ca22fbfd7e
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-11 08:35:06 -0800

    Foydalanuvchi yangi reja qo'shganida, loyihangiz rejani aynan shu foydalanuvchiga tegishli qilib saqlash

--- asosiy/models.py
+++ asosiy/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-HOLATI = [
-    ("Not started", "Not started"),
-    ("In Progress", "In Progress"),
-    ("Completed", "Completed"),
-]
 
+# HOLATI = [
+#     ("Not started", "Not started"),
+#     ("In Progress", "In Progress"),
+#     ("Completed", "Completed"),
+# ]
+#
 
 class Reja(models.Model):
     name = models.CharField(max_length=50)
     date = models.DateField(blank=True)
     details = models.TextField()
-    status = models.CharField(max_length=20, choices=HOLATI)
+    status = models.CharField(max_length=20)
+    egasi = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.task
+        return self.name
--- asosiy/views.py
+++ asosiy/views.py
@@ -1,12 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+from .models import *
 
 
-def home(requests):
-    return render(requests, "index.html")
+def rejalar(requests):
+    if requests.user.is_authenticated:
+        if requests.method == 'POST':
+            Reja.objects.create(
+                name=requests.POST.get("name"),
+                date=requests.POST.get("date"),
+                details=requests.POST.get("details"),
+                status=requests.POST.get("status"),
+                egasi=requests.user
+            )
+        content = {
+            "rejalar": Reja.objects.filter(egasi=requests.user),
+            "foydalanuvchi": requests.user.username.capitalize()
+        }
+        return render(requests, "index.html", content)
+    return redirect("/")
 
 
-def login(requests):
+def login_view(requests):
+    if requests.method == 'POST':
+        user = authenticate(
+            username=requests.POST.get("l"),
+            password=requests.POST.get("p")
+        )
+        if user is None:
+            return redirect("/")
+        login(requests, user)
+        return redirect("/rejalar/")
     return render(requests, "login.html")
+
+
+def logout_view(requests):
+    logout(requests)
+    return redirect("/")
 
 
 def edit(requests):
