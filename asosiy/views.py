8bf4c00e9b10c2f128d5e7fd01b4f1637c59b648
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-11-12 20:54:03 -0800

    viewsni tahrirlash dasturga aloqasi yoq

--- asosiy/views.py
+++ asosiy/views.py
@@ -4,39 +4,39 @@ from django.contrib.auth import authenticate, login, logout
 from .models import *
 
 
-def rejalar(requests):
-    if requests.user.is_authenticated:
-        if requests.method == 'POST':
+def rejalar(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
             Reja.objects.create(
-                name=requests.POST.get("name"),
-                date=requests.POST.get("date"),
-                details=requests.POST.get("details"),
-                status=requests.POST.get("status"),
-                egasi=requests.user
+                name=request.POST.get("name"),
+                date=request.POST.get("date"),
+                details=request.POST.get("details"),
+                status=request.POST.get("status"),
+                egasi=request.user
             )
         content = {
-            "rejalar": Reja.objects.filter(egasi=requests.user),
-            "foydalanuvchi": requests.user.username.capitalize()
+            "rejalar": Reja.objects.filter(egasi=request.user),
+            "foydalanuvchi": request.user.username.capitalize()
         }
-        return render(requests, "index.html", content)
+        return render(request, "index.html", content)
     return redirect("/")
 
 
-def login_view(requests):
-    if requests.method == 'POST':
+def login_view(request):
+    if request.method == 'POST':
         user = authenticate(
-            username=requests.POST.get("l"),
-            password=requests.POST.get("p")
+            username=request.POST.get("l"),
+            password=request.POST.get("p")
         )
         if user is None:
             return redirect("/")
-        login(requests, user)
+        login(request, user)
         return redirect("/rejalar/")
-    return render(requests, "login.html")
+    return render(request, "login.html")
 
 
-def logout_view(requests):
-    logout(requests)
+def logout_view(request):
+    logout(request)
     return redirect("/")
 
 
@@ -46,5 +46,5 @@ def reja_ochir(request, son):
     return redirect("/rejalar/")
 
 
-def edit(requests):
-    return render(requests, "edit.html")
+def edit(request):
+    return render(request, "edit.html")
